6b07d8ecbbd9f2b3f7a79a23d393172bc5f4babc
prasadkrn83 <38817885+prasadkrn83@users.noreply.github.com>
2018-12-12 21:53:59 -0600

    added commands
    added commands

--- background.js
+++ background.js
@@ -15,7 +15,7 @@ var hello = function(tag) {
 
 function openOptions() {
 chrome.runtime.openOptionsPage();
-chrome.runtime.cl
+//chrome.runtime.cl
 
     // chrome.app.window.create('vcat.html', {
     //     bounds: {
@@ -30,40 +30,96 @@ chrome.runtime.cl
 chrome.tabs.onCreated.addListener(function(tabs) {
 
     if (annyang && !annyang.isListening()) {
-        // alert('hello...');
-
-        // Let's define a command.
+     
         var commands = {
-            'hi': hello
+            'select all *type': function(type) {
+                console.log('calling from command..select all '+type);
+                performAction('select all '+type);
+            },
+            'select link *desc': function(desc) {
+                console.log('calling from command..select link '+desc);
+                performAction('select link $ '+desc);
+            },
+            'click link number *num': function(num){
+
+                num=(num=='one')?1:num;
+                console.log('calling from command..click link number '+num);
+                performAction('click link number '+num);
+            },
+            'click link *desc':function(desc){
+                console.log('calling from command..click link '+desc);
+                performAction('click link $ '+desc);
+            },
+            'scroll page *direction once':function(direction){
+                console.log('calling from command..scroll page '+direction);
+                performAction('scroll page '+direction +' once');
+            },
+            'scroll page *direction':function(direction){
+                console.log('calling from command..scroll page '+direction);
+                performAction('scroll page $ '+direction);
+            }
         };
 
-        // Add our commands to annyang
         annyang.addCommands(commands);
-        annyang.start({ autoRestart: true });
-        //alert('hello...');
-
+        annyang.start({ autoRestart: true ,continuous:false});
+     
         annyang.addCallback('soundstart', function() {
-            //alert('sound detected');
+            console.log('sound detected');
         });
 
         annyang.addCallback('result', function() {
-            // alert('sound stopped');
+             console.log('sound stopped');
         });
-        annyang.addCallback('result', function(phrases) {
+        annyang.addCallback('resultNoMatch', function(phrases) {
             console.log("I think the user said: ", phrases[0]);
             var inputStr  = phrases[0];
-            var res = inputStr.split(" ");
+            console.log('called here...');
+          /*  var res = inputStr.split(" ");
+      
             var message = { action: res[0],type1:res[1],type2:res[2] ,idenstr:null};
-
-            chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id,message , function(response) {
-                console.log(response.message);
-            });
-        });
+            chrome.tabs.query({ active: true,currentWindow: true }, function(tabs)             {
+                if((typeof tabs[0] === 'undefined') || (typeof tabs[0].title === 'undefined')){
+                    console.log('not a webpage');
+                    return;
+                }else{
+                    chrome.tabs.sendMessage(tabs[0].id,message , function(response) {
+                        console.log(response.message);
+                    });
+                }
+            }); */ 
+            performAction(inputStr);
+          
+            /*chrome.tabs.query({ active: true, windowType:"normal",currentWindow: true }, function(tabs) {
+                chrome.tabs.sendMessage(tabs[0].id,message , function(response) {
+                    console.log(response.message);
+                });
+            });*/
             console.log("But then again, it could be any of the following: ", phrases);
         });
 
-        // Start listening.
     }
 
 });
+
+function performAction(inputStr){
+
+    var commandStr = inputStr.split(" ");
+    var idenStr=null;
+    if(commandStr.length>3){
+        idenStr=commandStr[3];
+    }
+    console.log(commandStr);
+      
+     var message = { action: commandStr[0],type1:commandStr[1],type2:commandStr[2] ,idenstr:idenStr};
+            chrome.tabs.query({ active: true,currentWindow: true }, function(tabs)             {
+                if((typeof tabs[0] === 'undefined') || (typeof tabs[0].title === 'undefined')){
+                    console.log('not a webpage');
+                    return;
+                }else{
+                    chrome.tabs.sendMessage(tabs[0].id,message , function(response) {
+                        console.log(response.message);
+                    });
+                }
+            }); 
+
+}
--- content.js
+++ content.js
@@ -17,44 +17,88 @@ $.toast({
     icon: 'info'
 });
 
-scrollSmoothToBottom(null,false);
-
+    
       var action = request.action;
       var type1  = request.type1;
       var type2 =  request.type2;
       var idenstr  =  request.idenstr;
 
       var identifier="";
+      var i=0;
       if(action =="select"){
         if(type1=="all"){
           if(type2=="links"){
             identifier ="a";
+          }else if (type2=="drop down" || type2 == "list"){
+            identifier="select";
+          }else if(type2="text input"){
+            identifier="input[@type=\"text\"]";
+          }else if(type2="radio button"){
+            identifier="'input:radio'";
           }
-          $(identifier).css({"border-color": "red", 
-    "border-width":"3px", 
-    "border-style":"solid"});
-      
+          $(identifier).each(function(index,item){
+
+             i++;
+             console.log(item);
+             $(item).wrap("<fieldset id='f"+i+"'class='fldset-class'></fieldset>");
+             $("#f"+i).prepend("<legend class='legend-class'>"+i+"</legend>");
+       
+      });
         }else{
-
-          identifier = type2;
-          $(identifier).filter(function(index) { return $(this).text().indexOf(idenstr)>0; })[0].css({"border-color": "red", 
-    "border-width":"3px", 
-    "border-style":"solid"});
-
+          if(type1=="link"){
+            identifier ="a";
+          }else if (type1=="drop down" || type1 == "list"){
+            identifier="select";
+          }else if(type1="text input"){
+            identifier="input[@type=\"text\"]";
+          }else if(type1="radio button"){
+            identifier="'input:radio'";
+          }
+          $(identifier).filter(function(index) { return $(this).text().toLowerCase().indexOf(idenstr)>0; })[0].css({"border-color": "red", 
+            "border-width":"3px", 
+             "border-style":"solid"});
         }
       }else if(action =="click"){
-          identifier = type2;
-         $(identifier).filter(function(index) { 
-          console.log($(this).text());
-          return $(this).text().indexOf(idenstr)>0; })[0].click();
-      }
+        if(type1=='link'){
+            if(type2!=null && type2==='number'){
+                identifier='#f'+idenstr+' a';
+                console.log(identifier);
+                $(identifier)[0].click();
+            }else if(type2!=null && type2=='$'){
+               identifier = 'a';
+                 $(identifier).filter(function(index) { 
+                 console.log($(this).text());
+                 return $(this).text().toLowerCase().indexOf(idenstr)>0; })[0].click();
+               }       
+          }
+        }else if(action=='scroll'){
+          if(type1=='page'){
+            if(type2=='$' && idenstr=='up'){
+              scrollSmoothToTop(true); 
+            }else if(type2=='$' &&  idenstr=='down'){
+              scrollSmoothToBottom(true); 
+            }else if(type2=='up' && idenstr=='once'){
+              scrollSmoothToTop(false); 
+            }else if(type2=='down' &&  idenstr=='once'){
+              scrollSmoothToBottom(false); 
+            }
+          }
+        }else if(action=="open")
+        {
+          if(type1="new tab"){
+            openNewTab(null);
+          }else if("new browser"){
+            openNewBrowser(null);
+          }
+
+        }
+      
 
         sendResponse({message: "success"});
     });
 
 
-//Require jQuery
-function scrollSmoothToBottom (id,isToend) {
+function scrollSmoothToBottom (isToend) {
   var scrollval;
   if(isToend){
       scrollval = document.body.scrollHeight;
@@ -62,15 +106,23 @@ function scrollSmoothToBottom (id,isToend) {
       var currentPos = $(window).scrollTop();
       var halfHeight = $(window).height();
       scrollval = currentPos + halfHeight;
-   }$(scrollingElement).animate({
+   }
+   $(scrollingElement).animate({
       scrollTop: scrollval//document.body.scrollHeight
    }, 500);
 }
 
-//Require jQuery
-function scrollSmoothToTop (id,isToTop) {
+function scrollSmoothToTop (isToTop) {
+  var scrollval;
+  if(isToTop){
+      scrollval = 0;
+  }else{
+      var currentPos = $(window).scrollTop();
+      var halfHeight = $(window).height();
+      scrollval = currentPos - halfHeight;
+   }
    $(scrollingElement).animate({
-      scrollTop: 0
+      scrollTop: scrollval
    }, 500);
 }
 
@@ -80,4 +132,55 @@ function generateToast(message){
     text: message,
     icon: 'info'
 });
-}
+  }
+function scrollSmoothToTopLeft(){
+  scrollSmoothToTop(true);
+$(scrollingElement).animate({
+      scrollLeft:0
+   }, 0);
+  }
+
+  function scrollSmoothToBottomRight(){
+  scrollSmoothToBottom(true);
+$(scrollingElement).animate({
+      scrollLeft:$(document).outerWidth()
+   }, 0);
+  }
+  function openLinkinNewTab(url){
+    window.open(url, '_blank'); 
+  }
+  function closeWindow(){
+    window.close();
+  }
+  function minimizeWindow(id){
+    chrome.windows.update(id, { state: 'minimized' });
+  }
+  function maximizeWindow(id){
+    chrome.windows.update(id, { state: 'maximized' });
+  }
+  function refreshPage(){
+        location.reload();
+
+  }
+  function addToBookmark(url,title){
+
+          chrome.bookmarks.create({'parentId': extensionsFolderId,
+                               'title': title,
+                               'url': url});
+
+  }
+  function openNewTab(link){
+    if(link==null){
+      chrome.tabs.create();
+    }else{
+      chrome.tabs.create({url:link});
+    }
+  }
+function openNewBrowser(link){
+    if(link==null){
+      chrome.windows.create({CreateType:'normal'});
+    }else{
+      chrome.tabs.create({CreateType:'normal',url:link});
+    }
+  }
+
